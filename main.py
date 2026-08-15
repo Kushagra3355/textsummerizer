fc57c6189e38178027325a6b1ad451f21891ebb7
Kushagra3355 <kushagra.omar@gmail.com>
2026-08-15 21:50:55 +0530

    compeleted data transformation segment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.textsummarizer.logging import logger
 from src.textsummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.textsummarizer.pipeline.stage_2_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME="Data Ingestion stage"
 
@@ -12,3 +13,14 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+STAGE_NAME="Data Transformation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline=DataTransformationTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_transformation()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -1,7 +1,7 @@
 from src.textsummarizer.constants import CONFIG_FILE_PATH,PARAMS_FILE_PATH
 from src.textsummarizer.utils.common import read_yaml, create_directories
 
-from src.textsummarizer.entity import DataIngestionConfig
+from src.textsummarizer.entity import DataIngestionConfig,DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(self, config_path = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
@@ -22,3 +22,16 @@ class ConfigurationManager:
             
         )
         return data_ingestion_config
+    
+    def get_data_transformation_config(self)-> DataTransformationConfig:
+            config=self.config.data_transformation
+    
+            create_directories([config.root_dir])
+    
+            data_transformation_config=DataTransformationConfig(
+                root_dir=config.root_dir,
+                data_path=config.data_path,
+                tokenizer_name=config.tokenizer_name
+            )
+    
+            return data_transformation_config
